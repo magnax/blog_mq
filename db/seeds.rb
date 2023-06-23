0cd85713ae8b960ec150808e3efad6f1a7793abd
Maciej N <magnax@gmail.com>
2023-06-23 15:21:15 +0200

    Add some random content to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Generate 10 sample users
+(1..10).each do
+  User.create(
+    email: Faker::Internet.email,
+    password: '12345678',
+  )
+end
+
+# Generate 15 blogs
+(1..15).each do
+  Blog.create(
+    user: User.all.sample,
+    title: Faker::Book.title,
+  )
+end
+
+# Generate 30 blog posts
+(1..30).each do
+  Post.create(
+    blog: Blog.all.sample,
+    title: Faker::Lorem.sentence.titleize,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+# Generst 20 comments
+(1..20).each do
+  Comment.create(
+    post: Post.all.sample,
+    user: User.all.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
